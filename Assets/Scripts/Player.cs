a4b431864e4836e0787250a8cf063e27e97d09c2
Andy Lockhart <andy@mangledmonster.com.au>
2019-08-04 11:26:40 +1000

    188-Flip_Character_Sprite

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,6 +24,9 @@ public class Player : MonoBehaviour
         xPos = Input.GetAxis("Horizontal") * speed;
         this.transform.Translate(xPos * Time.deltaTime, 0, 0);
         */
+
+        Run();
+        FlipSprite();
     }
 
     private void Run()
@@ -31,5 +34,19 @@ public class Player : MonoBehaviour
         float controlThow = Input.GetAxis("Horizontal") * runSpeed; // value is between -1 and +1
         Vector2 playerVelocity = new Vector2(controlThow, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            /* My way
+            float xScale = Mathf.Sign(Input.GetAxis("Horizontal"));
+            transform.localScale = new Vector3(xScale, 1f, 1f);
+            */
+
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
     }
 }
